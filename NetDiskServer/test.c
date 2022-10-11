02f1d174cd9a8b9591228d78d3f23e95015d1071
xmd <1215047618@qq.com>
2022-10-11 16:06:34 +0800

    xmd:xmd.h

---
+++ NetDiskServer/test.c
@@ -0,0 +1,15 @@
+#include "xmd.h"
+
+int main(void)
+{
+     char* dir ="~/NetDiskServer";
+
+    char path[] = "~/cpp46";
+    
+    ls_t(dir);
+    //cd_t(path);
+    //getcwd_t();
+    
+    return 0;
+}
+
---
+++ NetDiskServer/xmd.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <func.h>
+
+void cd_t(char *path);
+
+void ls_t(char* dir);
+
+void getcwd_t(void);
